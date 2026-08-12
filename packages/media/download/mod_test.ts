d5f568dc84e7449b969ffcb662359752455ef5ec
Okiki Ojo <hey@okikio.dev>
2026-08-12 19:13:39 -0400

    chore: remove workflow utility package and associated type definitions
    Signed-off-by: Okiki Ojo <hey@okikio.dev>

--- packages/media/download/mod_test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, it } from 'node:test';
-import { expect } from '@std/expect';
-import { DownloadSchema } from './mod.ts';
-
-describe('@media/download', () => {
-	it('validates direct download input', () => {
-		expect(DownloadSchema.parse({ url: 'https://example.com/file.mp4' }).headers).toEqual({});
-	});
-});
--- utils/http/problem/testing/mod.ts
+++
@@ -1,5 +0,0 @@
-export {
-	coverage,
-	ProblemCoverageError,
-	type ProblemCoverageReport,
-} from './coverage.ts';
